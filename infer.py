3a1d7167f3ad9891d1fe77db94ea75419bede9b6
prophesier <37784682+prophesier@users.noreply.github.com>
2022-11-13 21:32:07 +0800

    improve support for 44.1kHz

--- infer.py
+++ infer.py
@@ -15,6 +15,7 @@ chunks_dict = infer_tool.read_temp("./infer_tools/chunks_temp.json")
 
 def run_clip(svc_model, key, acc, use_pe, use_crepe, thre, use_gt_mel, add_noise_step, project_name='', f_name=None,
              file_path=None, out_path=None):
+    use_pe=use_pe if hparams['audio_sample_rate']==24000 else False
     if file_path is None:
         raw_audio_path = f"./raw/{f_name}"
         clean_name = f_name[:-4]
